1bb4a735e221f26ad818eb51e8696282f940952d
Marcos Frech <Marcos Frech>
2023-06-06 17:41:34 -0600

    login mejorado

--- react-client/src/pages/login/index.jsx
+++ react-client/src/pages/login/index.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import "./style.css"; // Import the external CSS file
 
 export default function Login({ onLogin }) {
@@ -12,58 +11,56 @@ export default function Login({ onLogin }) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Invoke the onLogin function passing the login data
     onLogin(username, password);
   };
 
   return (
-    <Container maxWidth="xs" style={{ marginTop: 40 }}>
-      <Typography
-        variant="h4"
-        component="h1"
-        align="center"
-        gutterBottom
-        className="heading"
-      >
-        Página de inicio
-      </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          fullWidth
-          type="text"
-          label="Nombre de usuario"
-          variant="outlined"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-          margin="normal"
-          className="text-input" // Add custom CSS class
-        />
-        <TextField
-          fullWidth
-          type="password"
-          label="Contraseña"
-          variant="outlined"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          margin="normal"
-          className="text-input" // Add custom CSS class
-        />
-
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          fullWidth
-          size="large"
-          sx={{ marginTop: "1rem" }}
-          className="submit-button" // Add custom CSS class
-        >
-          Iniciar sesión
-        </Button>
-      </form>
-      <div style={{ marginTop: "1rem", textAlign: "center" }}>
-        <Link to="/register"> Registro </Link>
+    <div className="container">
+      <div className="left-section">
+        <img src="logo192.png" alt="Login" className="login-image" />
       </div>
-    </Container>
+      <div className="right-section">
+        <Typography variant="h4" align="center" gutterBottom className="heading">
+          Página de inicio
+        </Typography>
+        <form onSubmit={handleSubmit} className="form">
+          <TextField
+            fullWidth
+            type="text"
+            label="Nombre de usuario"
+            variant="outlined"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            margin="normal"
+            className="text-input"
+          />
+          <TextField
+            fullWidth
+            type="password"
+            label="Contraseña"
+            variant="outlined"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            margin="normal"
+            className="text-input"
+          />
+
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            size="large"
+            sx={{ marginTop: "1rem" }}
+            className="submit-button"
+          >
+            Iniciar sesión
+          </Button>
+        </form>
+        <div style={{ marginTop: "1rem", textAlign: "center" }}>
+          <Link to="/register">Registro</Link>
+        </div>
+      </div>
+    </div>
   );
 }
